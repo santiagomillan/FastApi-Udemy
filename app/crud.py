f47fe76b4d86171d1e59f5b48d2f6b49bb46d1d7
Santiago Millan <santiagomillanpardo@gmail.com>
2025-12-30 18:15:29 -0500

    feature: Crud categoria

--- app/crud.py
+++ app/crud.py
@@ -1,14 +1,10 @@
 from sqlalchemy.orm import Session 
-from models import Producto
+from models import Producto, Categoria
 from schemas import *
 # from models.producto import Producto
 # from schemas.producto import ProductoCreate
 
-
-
-
-
-
+### Producto CRUD operations
 
 def crear_producto(db: Session, producto:ProductoCreate):
     db_producto = Producto(**producto.dict())
@@ -17,17 +13,11 @@ def crear_producto(db: Session, producto:ProductoCreate):
     db.refresh(db_producto)
     return db_producto
 
-
-
-
-
 def obtener_productos(db:Session):
     return db.query(Producto).all()
 
-
 def obtener_producto(db:Session, producto_id: int ):
     return db.query(Producto).filter(Producto.id == producto_id).first()
-
 
 def actualizar_producto(db:Session, producto_id: int, datos:ProductoCreate):
     producto = obtener_producto(db, producto_id)
@@ -46,3 +36,15 @@ def eliminar_producto(db:Session, producto_id: int):
         db.delete(producto)
         db.commit()
     return producto
+
+### Categoria CRUD operations
+
+def crear_categoria(db: Session, categoria:CategoriaCreate):
+    db_categoria = Categoria(nombre = categoria.nombre)
+    db.add(db_categoria)
+    db.commit()
+    db.refresh(db_categoria)
+    return db_categoria
+
+def obtener_categorias(db:Session):
+    return db.query(Categoria).all()
