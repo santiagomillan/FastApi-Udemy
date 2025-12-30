879f4f0255609229b1685ad5195d3f857aa7cf4a
Santiago Millan <santiagomillanpardo@gmail.com>
2025-12-30 18:10:33 -0500

    feature: Schema categoria

--- app/schemas.py
+++ app/schemas.py
@@ -10,8 +10,18 @@ class ProductoCreate(BaseModel):
     stock: int
     categoria_id: int
 
-
 class ProductoResponse(ProductoCreate):
     id: int 
+    class Config:
+        orm_mode = True
+
+class CategoriaBase(BaseModel):
+    nombre: str
+
+class CategoriaCreate(CategoriaBase):
+    pass
+
+class CategoriaResponse(CategoriaBase):
+    id: int
     class Config:
         orm_mode = True
