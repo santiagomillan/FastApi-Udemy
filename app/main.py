1b953849cd52f43d99ee89cf771f396702c96575
Santiago Millan <santiagomillanpardo@gmail.com>
2025-12-31 21:29:05 -0500

    feature:  Crear enpoint login y crear usuario

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 # from pydantic import BaseModel
 from sqlalchemy.orm import Session
 import crud, schemas
 from database import get_db
-
+from fastapi.security import OAuth2PasswordRequestForm
+from utils import verify_password
+from auth import crear_token
+from deps import get_current_user , require_admin
 
 app = FastAPI()
 
@@ -43,4 +46,21 @@ def crear_categoria(categoria: schemas.CategoriaCreate, db: Session = Depends(ge
 @app.get("/categorias", response_model=list[schemas.CategoriaResponse])
 def listar_categorias(db: Session = Depends(get_db)):
     return crud.obtener_categorias(db) 
+
+## Usuarios
+@app.post("/usuarios", response_model=schemas.UsuarioResponse, status_code=status.HTTP_201_CREATED)
+def registrar_usuario(usuario: schemas.UsuarioCreate, db:Session = Depends(get_db)):
+    try:
+        return crud.crear_usuario(db, usuario)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.post("/login", response_model=schemas.Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session =Depends(get_db)):
+    user = crud.obtener_usuario_por_email(db, form_data.username)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Credenciales invalidas")
+    token = crear_token(sub=user.email, es_admin=user.es_admin)
+    return {"access_token": token, "token_type": "bearer"}
+
 
--- app/schemas.py
+++ app/schemas.py
@@ -41,3 +41,7 @@ class UsuarioResponse(UsuarioBase):
     es_admin: bool
     class Config:
         orm_mode = True    
+
+class Token(BaseModel):
+    access_token : str
+    token_type: str = "bearer"
